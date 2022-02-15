b2894f914c73c522c858b765278e61d6132703a6
gilsgil <gilsgil@gmail.com>
2022-02-14 21:13:55 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/shadowlab/cursomc/domain/Categoria.java
+++ src/main/java/com/shadowlab/cursomc/domain/Categoria.java
@@ -1,12 +1,17 @@
 package com.shadowlab.cursomc.domain;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
 
 	public Categoria() {
