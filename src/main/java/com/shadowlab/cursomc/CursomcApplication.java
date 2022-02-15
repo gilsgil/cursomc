9df4dac849c748f36e95ef4295922450ac229fb4
gilsgil <gilsgil@gmail.com>
2022-02-14 22:25:34 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/shadowlab/cursomc/CursomcApplication.java
+++ src/main/java/com/shadowlab/cursomc/CursomcApplication.java
@@ -1,13 +1,16 @@
 package com.shadowlab.cursomc;
 
 import com.shadowlab.cursomc.domain.Categoria;
+import com.shadowlab.cursomc.domain.Produto;
 import com.shadowlab.cursomc.repositories.CategoriaRepository;
+import com.shadowlab.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -15,15 +18,31 @@ public class CursomcApplication implements CommandLineRunner {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(List.of(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
---
+++ src/main/java/com/shadowlab/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.shadowlab.cursomc.repositories;
+
+import com.shadowlab.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
