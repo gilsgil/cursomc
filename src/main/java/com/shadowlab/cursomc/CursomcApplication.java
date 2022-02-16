7b92231f85bf23d0094ef07e8d2c38010f3ab83b
gilsgil <gilsgil@gmail.com>
2022-02-15 21:09:07 -0300

    Estado e Cidade

--- src/main/java/com/shadowlab/cursomc/CursomcApplication.java
+++ src/main/java/com/shadowlab/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.shadowlab.cursomc;
 
 import com.shadowlab.cursomc.domain.Categoria;
+import com.shadowlab.cursomc.domain.Cidade;
+import com.shadowlab.cursomc.domain.Estado;
 import com.shadowlab.cursomc.domain.Produto;
 import com.shadowlab.cursomc.repositories.CategoriaRepository;
+import com.shadowlab.cursomc.repositories.CidadeRepository;
+import com.shadowlab.cursomc.repositories.EstadoRepository;
 import com.shadowlab.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -21,6 +25,12 @@ public class CursomcApplication implements CommandLineRunner {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
@@ -38,11 +48,25 @@ public class CursomcApplication implements CommandLineRunner {
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
         cat2.getProdutos().addAll(List.of(p2));
 
-        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p1.getCategorias().addAll(List.of(cat1));
         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-        p3.getCategorias().addAll(Arrays.asList(cat1));
+        p3.getCategorias().addAll(List.of(cat1));
+
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(List.of(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 }
---
+++ src/main/java/com/shadowlab/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.shadowlab.cursomc.repositories;
+
+import com.shadowlab.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
